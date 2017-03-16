70d7af33c4e45f520a74d481f35a5f2a349b17c8
Alexis Dubus <ielow.submarine@gmail.com>
2017-03-16 15:17:26 +0100

    rajout du spinner

--- app/src/main/java/com/alexis_dubus/nibral2/restaurant.java
+++ app/src/main/java/com/alexis_dubus/nibral2/restaurant.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class restaurant extends AppCompatActivity {
 
@@ -16,6 +18,13 @@ public class restaurant extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Spinner  spinner = (Spinner) findViewById(R.id.spinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.xml.cuisine, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
     }
 
